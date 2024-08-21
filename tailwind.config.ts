1e349d7db7b64530504e39dee8fb43d121adba18
Wisnu Ginanjar Saputra <wisnu@zero-one-group.com>
2024-08-21 14:40:04 +0700

    feat: setup project & create page control component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        sans: ['var(--font-sans)'],
+        serif: ['var(--font-serif)']
+      },
+      colors: {
+        'background': '#fbf9f4'
       },
     },
   },
